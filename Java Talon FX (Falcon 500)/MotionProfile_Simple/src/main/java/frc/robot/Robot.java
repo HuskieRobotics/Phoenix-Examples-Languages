0aec35ead7dcf7fa3cadaadd305d4d5e53e196bd
Geoff Schmit <gcschmit@gmail.com>
2023-03-14 20:27:55 -0500

    scale down joystick input

--- Java Talon FX (Falcon 500)/MotionProfile_Simple/src/main/java/frc/robot/Robot.java
+++ Java Talon FX (Falcon 500)/MotionProfile_Simple/src/main/java/frc/robot/Robot.java
@@ -130,7 +130,7 @@ public class Robot extends TimedRobot {
         switch (_state) {
             /* drive master talon normally */
             case 0:
-                _master.set(TalonFXControlMode.PercentOutput, axis);
+                _master.set(TalonFXControlMode.PercentOutput, axis * 0.2);
                 if (bFireMp == true) {
                     /* go to MP logic */
                     _state = 1;
