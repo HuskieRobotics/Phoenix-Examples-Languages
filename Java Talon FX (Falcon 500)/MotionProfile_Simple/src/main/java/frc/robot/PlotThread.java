20b1b33944cc3449e64c4531c7c11fba128d2c31
Geoff Schmit <gcschmit@gmail.com>
2023-03-15 22:45:08 -0500

    add volts

--- Java Talon FX (Falcon 500)/MotionProfile_Simple/src/main/java/frc/robot/PlotThread.java
+++ Java Talon FX (Falcon 500)/MotionProfile_Simple/src/main/java/frc/robot/PlotThread.java
@@ -37,22 +37,26 @@ class PlotThread implements Runnable {
 			/* Grab the latest signal update from our 1ms frame update */
 			double sen_pos = _rotationTalon.getSelectedSensorPosition(0);
 			double sen_vel = _rotationTalon.getSelectedSensorVelocity(0);
+			double mtr_volts = _rotationTalon.getMotorOutputVoltage();
 			double trgt_pos = _rotationTalon.getActiveTrajectoryPosition(0);
 			double trgt_vel = _rotationTalon.getActiveTrajectoryVelocity(0);
 			double trgt_arbF = _rotationTalon.getActiveTrajectoryArbFeedFwd(0);
 			SmartDashboard.putNumber("rot_sen_pos", sen_pos);
 			SmartDashboard.putNumber("rot_sen_vel", sen_vel);
+			SmartDashboard.putNumber("rot_mtr_volts", mtr_volts);
 			SmartDashboard.putNumber("rot_trgt_pos", trgt_pos);
 			SmartDashboard.putNumber("rot_trgt_vel", trgt_vel);
 			SmartDashboard.putNumber("rot_trgt_arbF", trgt_arbF);
 
 			sen_pos = _extensionTalon.getSelectedSensorPosition(0);
 			sen_vel = _extensionTalon.getSelectedSensorVelocity(0);
+			mtr_volts = _extensionTalon.getMotorOutputVoltage();
 			trgt_pos = _extensionTalon.getActiveTrajectoryPosition(0);
 			trgt_vel = _extensionTalon.getActiveTrajectoryVelocity(0);
 			trgt_arbF = _extensionTalon.getActiveTrajectoryArbFeedFwd(0);
 			SmartDashboard.putNumber("ext_sen_pos", sen_pos);
 			SmartDashboard.putNumber("ext_sen_vel", sen_vel);
+			SmartDashboard.putNumber("ext_mtr_volts", mtr_volts);
 			SmartDashboard.putNumber("ext_trgt_pos", trgt_pos);
 			SmartDashboard.putNumber("ext_trgt_vel", trgt_vel);
 			SmartDashboard.putNumber("ext_trgt_arbF", trgt_arbF);
